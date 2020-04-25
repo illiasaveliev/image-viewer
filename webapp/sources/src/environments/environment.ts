7cfac1dff20b3b952ef89795a037989a3c655182
Illia Saveliev <illiasaveliev@gmail.com>
2020-04-25 16:03:59 +0300

    Added new version

--- webapp/sources/src/environments/environment.ts
+++ webapp/sources/src/environments/environment.ts
@@ -1,4 +1,2 @@
-// import { assign } from 'lodash';
-// export const environment = assign((<any>window)._env, {});
-
-export const environment = (<any>window)._env;
+import { assign } from 'lodash';
+export const environment = assign((<any>window)._env, {});
