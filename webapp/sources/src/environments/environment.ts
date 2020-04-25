19efdd60f3bf7fe2af682a2500b120fc5a09875d
Illia Saveliev <illiasaveliev@gmail.com>
2020-04-25 15:41:02 +0300

    Removed prod config

--- webapp/sources/src/environments/environment.ts
+++ webapp/sources/src/environments/environment.ts
@@ -1,2 +1,4 @@
-import { assign } from 'lodash';
-export const environment = assign((<any>window)._env, {});
+// import { assign } from 'lodash';
+// export const environment = assign((<any>window)._env, {});
+
+export const environment = (<any>window)._env;
